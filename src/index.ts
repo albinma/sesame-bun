f1ca618bbc56b7f3de9d56cce381e2c67d715a0c
Albin Ma <albinma@gmail.com>
2023-09-11 10:57:03 -0400

    Add logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+import { logger } from '@bogeychan/elysia-logger';
 import { Elysia } from 'elysia';
 
 const port = Bun.env.HTTP_PORT || 8080;
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(port);
+const app = new Elysia()
+  .use(
+    logger({
+      level: 'debug',
+    }),
+  )
+  .get('/', (ctx) => {
+    ctx.log.error(ctx, 'Context');
+    ctx.log.info(ctx.request, 'Request'); // noop
+    return 'Hello Elysia';
+  })
+  .listen(port);
 
+// eslint-disable-next-line no-console
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
