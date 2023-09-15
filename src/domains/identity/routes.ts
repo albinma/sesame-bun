5f36e37aa537b82ea6db90b709b4643f5875ab0f
Albin Ma <albinma@gmail.com>
2023-09-15 18:00:44 -0400

    Add openapi initial spec

--- src/domains/identity/routes.ts
+++
@@ -1,8 +0,0 @@
-import { register, verify } from '@/domains/identity/handlers';
-import { setup } from '@/global/setup';
-import Elysia from 'elysia';
-
-export const authRoutes = new Elysia()
-  .use(setup)
-  .post('/register', register)
-  .post('/verify', verify);
