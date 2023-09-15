7c035ae9e00e1d636ec9bb77d151a1007df3b419
Albin Ma <albinma@gmail.com>
2023-09-15 01:50:03 -0400

    Add request logging

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,10 +1,23 @@
+import { logger } from '@/shared/initializers/logger';
 import express, { Express, Request, Response } from 'express';
+import { pinoHttp } from 'pino-http';
+import { v4 } from 'uuid';
 
 // Disabling require return type rule for this because of the nature of chaining API derived types.
 export const setupApp = (): Express => {
   const app = express();
 
-  app.get('/', (req: Request, res: Response) => res.send('ok'));
+  app.use(
+    pinoHttp({
+      logger,
+      genReqId: () => v4(),
+    }),
+  );
+
+  app.get('/', (req: Request, res: Response) => {
+    req.log.info('hello world');
+    res.send('ok');
+  });
 
   return app;
 };
