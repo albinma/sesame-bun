c9e42773587ad561dffbcd3cd7366c91d6f888c2
Albin Ma <albinma@gmail.com>
2023-09-15 09:33:29 -0400

    Improve app startup logging

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { setupApp } from '@/global/app';
+import { logger } from '@/shared/initializers/logger';
+import { AddressInfo } from 'ws';
 
 const port = Number(Bun.env.HTTP_PORT) || 8080;
 const app = setupApp();
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`sesame-bun listening on port ${port}`);
+const server = app.listen(port, () => {
+  logger.info('sesame-bun started');
 });
+
+const address = server.address() as AddressInfo;
+logger.info(`sesame-bun starting on http://${address.address}:${port}`);
