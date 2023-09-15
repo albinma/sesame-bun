5f45cf4b791563856d2d19d28ad9ac864d0dcfd9
Al <albinma@gmail.com>
2023-09-15 12:52:30 -0400

    Add security headers to responses (#2)
    * Add base image on top of bun
    
    * Use node image and install bun
    
    * Test bun version after install
    
    * Add bun to env
    
    * Reload bash_env
    
    * Run tests
    
    * Add helmet and request id
    
    * Fix bun lock
    
    * Add security header tests

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,5 +1,6 @@
 import { logger } from '@/shared/initializers/logger';
 import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
 import { pinoHttp } from 'pino-http';
 import { v4 } from 'uuid';
 
@@ -8,9 +9,22 @@ export const setupApp = (): Express => {
   const app = express();
 
   app.use(
+    helmet({
+      // Disable CSP because it's not needed for this API only project.
+      contentSecurityPolicy: false,
+    }),
+  );
+
+  app.use((req, res, next) => {
+    req.id = v4();
+    res.set('X-Request-Id', req.id);
+    next();
+  });
+
+  app.use(
     pinoHttp({
       logger,
-      genReqId: () => v4(),
+      genReqId: (req) => req.id,
     }),
   );
 
