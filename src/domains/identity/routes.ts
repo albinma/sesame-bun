3bfe72a49cd3935337e5d213b2f0083d422ffe1f
Albin Ma <albinma@gmail.com>
2023-09-16 01:31:47 -0400

    Add other handlers

--- src/domains/identity/routes.ts
+++ src/domains/identity/routes.ts
@@ -1,10 +1,16 @@
-import { beginAuthentication } from '@/domains/identity/handlers';
+import {
+  beginAuthentication,
+  completeAuthentication,
+  refreshAuthentication,
+} from '@/domains/identity/handlers';
 import { Router } from 'express';
 
 export async function createIdentityRouter(): Promise<Router> {
   const router = Router();
 
   router.post('/auth/begin', beginAuthentication);
+  router.post('/auth/complete', completeAuthentication);
+  router.post('/auth/refresh', refreshAuthentication);
 
   return router;
 }
